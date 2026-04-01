9291d6cbe670138b71751a50fd0e602b77b7695e
ConnerDeFeo <ninjanerozz@gmail.com>
2026-04-01 13:22:33 -0400

    removing clutter

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 pip#!/bin/bash
 
-docker compose up -d
-docker compose wait aws-linux
-
 cd terraform
 terraform apply --auto-approve
-
-cd ../layers/twilio/python
-rm -rf aio** attr** bin cert** charset** frozen** idna** jwt multidict** propcache** pyjwt** requests** twilio** typing_extensions** urllib3** yarl** **.so
-
-cd ../../../
-docker compose down
+cd ..
