c559b3a0dc4a9fe03ba72ab41a41d88d4509d61a
ConnerDeFeo <ninjanerozz@gmail.com>
2026-04-04 08:37:58 -0400

    done

--- server/routes/rochester_roofing/rochester_roofing_websocket.py
+++ server/routes/rochester_roofing/rochester_roofing_websocket.py
@@ -3,8 +3,8 @@ import logging
 import asyncio
 from fastapi import APIRouter, WebSocket
 from config import APPOINTMENT_BOOKED_INDICATOR, DEFAULT_APPOINTMENT_DATA
-from server.dynamo import get_conversation_history, save_conversation, get_appointment_data
-from server.conversation import stream_conversation, run_extraction
+from dynamo import get_conversation_history, save_conversation, get_appointment_data
+from conversation import stream_conversation, run_extraction
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
