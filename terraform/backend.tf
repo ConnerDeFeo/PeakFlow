ed22715d267713ac2287ef959bf039ac30fba3f8
Conner DeFeo <ninjanerozz@gmail.com>
2026-04-05 07:56:05 -0400

    that should make it better

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+    backend "s3" {
+      bucket         = "ai-automation-tfstate"
+      key            = "terraform.tfstate"
+      region         = "us-east-2"
+      use_lockfile   = true
+      encrypt        = true
+    }
+}
