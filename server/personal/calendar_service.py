96acda790bea6bb8b65c6eee7e5e97710818b6a5
Conner DeFeo <ninjanerozz@gmail.com>
2026-04-06 10:47:52 -0400

    lets see

--- server/personal/calendar_service.py
+++ server/personal/calendar_service.py
@@ -67,7 +67,7 @@ def get_available_time_slots(time_min, time_max):
             current += timedelta(minutes=30)
         avail[str(date)] = slots
 
-    return avail, taken
+    return events_result
 
 
 def book_google_calendar_appointment(dt, summary, duration_minutes=60, description=""):
