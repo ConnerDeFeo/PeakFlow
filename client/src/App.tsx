305d32572afc372fe12d44d522221b645bd42ad0
Conner DeFeo <ninjanerozz@gmail.com>
2026-04-09 11:42:18 -0400

    please

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { NavBar } from './components/NavBar';
 import { Footer } from './components/Footer';
 import Contact from './pages/Contact';
-import Landing from './pages/Landing/Landing';
+import Landing from './pages/landing/Landing';
 
 function App() {
   return (
