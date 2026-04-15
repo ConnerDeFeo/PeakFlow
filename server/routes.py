fb72504d15fc731c261c6d46f305ca6185cba0b7
ConnerDeFeo <ninjanerozz@gmail.com>
2026-04-15 11:27:06 -0400

    changin the voice for adonis

--- server/routes.py
+++ server/routes.py
@@ -49,7 +49,7 @@ def incoming_call_route_adonis_roofing():
     return incoming_call(
         websocket_url=f"wss://{SERVER_DOMAIN}/{Client.ADONIS_ROOFING.value}/{WS}",
         welcome_greeting="Hi, this is Adonis Roofing, our receptionist is currently unavailable, I'm our AI assistant. How can I help you today?",
-        voice_name="gfRt6Z3Z8aTbpLfexQ7N"
+        voice_name="X03mvPuTfprif8QBAVeJ"
     )
 
 @router.websocket(f"/{Client.ADONIS_ROOFING.value}/{WS}")
